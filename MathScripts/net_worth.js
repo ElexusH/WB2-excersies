60cb74baabe56b8d3f2d50c6c468654228dfdd59
Elexus Harris <eharris@appdev.yearup.org>
2023-04-25 11:41:04 -0400

    networth

---
+++ MathScripts/net_worth.js
@@ -0,0 +1,11 @@
+// gather inputs
+var phone= 800;
+var car= 50000;
+var phonebill= 150;
+var card= 200;
+
+//perform calc
+var networth= phone+car-phonebill-card;
+
+//print output
+console.log ("your networth is "+ networth)
---
+++ tips.js
@@ -0,0 +1,10 @@
+// gather inputs
+var foodcost=79.25
+var tax=6.54
+var tip=12.00
+
+//perform calc
+var totaldue= foodcost+tax+tip;
+
+//output
+console.log("The Total due is ", totaldue)
